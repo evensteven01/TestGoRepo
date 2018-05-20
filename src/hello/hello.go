cb803a877d3a4c063fe527321b72344929028763
Steven Ford <evensteven01@gmail.com>
2018-05-20 00:23:25 -0400

    Added lambda handler

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
+)
+type MyEvent struct {
+    Name string 'json: "name"'
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+    return fmt.Sprintf("Hello %s!, name.Name), nil
+}
 
 func main() {
     fmt.Printf("Hello, world!\n")
