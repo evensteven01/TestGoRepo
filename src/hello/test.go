37f6a9081aa8514d9ad082909e29c5e9043a879f
Steven Ford <evensteven01@gmail.com>
2018-05-20 21:40:01 -0400

    Forgot to use var

--- src/hello/test.go
+++ src/hello/test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestDoSomething(t *testing.T) {
-    str = "Hello World!"
-    actual = AddYouSaid(str)
-    expected = "You said: " + str
+    var str string = "Hello World!"
+    var actual string = AddYouSaid(str)
+    var expected string = "You said: " + str
     if actual != expected {
         t.Errorf("Incorrect string returned. got: %s, expected: %s", actual, expected)
     }
