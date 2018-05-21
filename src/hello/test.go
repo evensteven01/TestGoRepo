72740f457d16a156c852920a41e6a327fdf01e9a
Steven Ford <evensteven01@gmail.com>
2018-05-20 21:32:51 -0400

    Fixed a missing equal sign

--- src/hello/test.go
+++ src/hello/test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestDoSomething(t *testing.T) {
     str = "Hello World!"
     actual = AddYouSaid(str)
-    expected "You said: " + str
+    expected = "You said: " + str
     if actual != expected {
         t.Errorf("Incorrect string returned. got: %s, expected: %s", actual, expected)
     }
